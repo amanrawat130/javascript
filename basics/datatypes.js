778643c71fdd8d5b811fc327e187bd6761c56537
amanrawat130 <imdemario@gmail.com>
2025-05-26 00:36:28 +0530

    new

---
+++ basics/datatypes.js
@@ -0,0 +1,13 @@
+const name = "AMan";
+const age = 18;
+
+const arr = [1,2,3,4,5]
+
+const obj = {
+    "name": "Aman",
+    "Age": 18
+}
+console.log(typeof obj);
+console.log(typeof arr);
+console.log(typeof name);
+console.log(typeof age);
